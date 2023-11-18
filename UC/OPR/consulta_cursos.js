fd4083e0d04ba538c2dc47b02a81cc76bb86f4b6
Tomas <tomaas.ra@gmail.com>
2023-11-18 18:01:03 -0300

    18-11-23

--- UC/OPR/consulta_cursos.js
+++ UC/OPR/consulta_cursos.js
@@ -1,52 +1,3 @@
-var facultades = {"Escuela de Ingeniería":
-                                      {"IIC3413": "Implementación de Sistemas de Base de Datos",
-                                       "IIC2714": "Fundamentos de Procesamiento de Imagenes",
-                                       "IIC2222": "Teoria de Automatas y Lenguajes Formales",
-                                       "IIC3783": "Procesamiento Avanzado de Imagenes",
-                                       "IIC2283": "Diseño y Análisis de Algoritmos",
-                                       "IIC3242": "Complejidad Computacional",
-                                       "IIC3633": "Sistemas Recomendadores",
-                                       "ICS3153": "Optimización Avanzada",
-                                       "IIC2142": "Ingenieria de Software",
-                                       "IIC3697": "Aprendizaje Profundo",
-                                       "ICS113H": "Optimización-Honors",
-                                       "IIC2733": "Modelos de Procesos"},
-
-            "Instituto de Ingeniería Matemática y Computacional":
-                                       {"IMT2112": "Algoritmos Paralelos en Computación Científica",
-                                       "IMT3120": "Fundamentos Matemáticos de Ciencia de Datos",
-                                       "IMT2115": "Control de Sistemas Lineales",
-                                       "IMT2565": "Optimización Combinatorial",
-                                       "IMT2113": "Análisis de Fourier Aplicado",
-                                       "IMT2111": "Álgebra Lineal Numérica"},
-
-            "Facultad de Matemáticas":
-                                       {"EYP2427": "Minería de Datos e Inteligencia de Negocios",
-                                       "EYP3307": "Modelos Lineales Generalizados",
-                                       "EYP3437": "Análisis Multivariado",
-                                       "MAT2605": "Cálculo Científico I",
-                                       "EYP3907": "Series de Tiempo",
-                                       "EYP2417": "Muestreo"},
-                                       
-            "Facultad de Ciencias Biológicas":
-                                       {"BIO318E": "Principios de Dinámica Poblacional: Teoría y Aplicaciones",
-                                       "BIO295A": "Seminario de Investigacion Departamental",
-                                       "BIO110C": "Biología de Organismos y Comunidades",
-                                       "BIO141C": "Biología de la Célula",
-                                       "BIO231C": "Ecología"},
-
-            "Escuela de Gobierno":   
-                                     {"GOB3023": "Taller de Análisis Aplicado de Política Pública",
-                                     "GOB3015": "Modelos de Decisión para Políticas Públicas"},
-
-            "Instituto de Geografía":
-                                       {"GEO202": "Sistemas de Información Geográfica",
-                                       "GEO309": "Percepción Remota Satelital"},
-
-            "Instituto de Astrofísica":
-                                       {"AST0212": "Introducción al Análisis de Datos"}
-}
-
 function redirigir(sigla) {
     let link = "https://catalogo.uc.cl/index.php?tmpl=component&option=com_catalogo&view=programa&sigla="
     window.open(link + sigla)
@@ -55,7 +6,7 @@ function redirigir(sigla) {
 function generarEnlace() {
     for (facultad of Object.keys(facultades)) {
         document.write('<div class="contenedor">');
-        document.write('<div class="facultades" style="padding-left:40px; padding-right:10px">');
+        document.write('<div class="facultades" style="padding-left:40px; padding-right:17px">');
         
         document.write('<h2>' + facultad + '</h2>');
         siglas = Object.keys(facultades[facultad]);
